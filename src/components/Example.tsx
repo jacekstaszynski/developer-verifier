49b4b537da4421156bebab4e25ebb0b964abff26
jacekstaszynski <jacek.staszynski_ext@scout24.com>
2022-07-13 08:43:21 +0200

    Emotion style added

---
+++ src/components/Example.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function Example() {
+  return <Header> Example </Header>;
+}
+
+const Header = styled.span({
+  color: "blue",
+  fontSize: 20,
+});
+
+export default Example;
