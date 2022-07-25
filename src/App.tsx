beac2420bcb8fb5b62035a2ff80b8f053023f77d
jacekstaszynski <jacek.staszynski_ext@scout24.com>
2022-07-25 08:10:35 +0200

    Code cleaned

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Example from "./components/Example";
 
 function App() {
   return (
     <div>
       <h1>Learn React</h1>
+      <Example />
     </div>
   );
 }
